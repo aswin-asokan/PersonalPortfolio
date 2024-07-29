d907ae4520bac9e322396b6f5404c01f2d79f98e
Aswin Asokan <86108610+aswin-asokan@users.noreply.github.com>
2024-07-29 08:09:54 +0530

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
           <h1 className="display-5">
             Hey I'm,<span style={{ color: "#8baac4" }}> Aswin</span>
           </h1>
-          <p>Self-Taught Application Developer | Artist | Student</p>
+          <p>Application Developer | Artist | Student</p>
           <p>
             I am a self-taught application developer passionate about creative
             problem-solving. I also love to draw while enjoying music. As a
